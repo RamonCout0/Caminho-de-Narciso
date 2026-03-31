c65085d11a79724231f4d889737d987e43962d3a
Igor Maikel <igormaikel2024@gmail.com>
2026-03-30 23:08:35 -0300

    porque nao ta funcionando

---
+++ Content/Scenes/Area2d.cs
@@ -0,0 +1,56 @@
+using Godot;
+using System;
+
+public partial class Teleporter : Area2d
+{
+	// O [Export] faz o mesmo que o @export do GDScript
+	[Export(PropertyHint.File, "*.tscn")]
+	public string ProximaCena;
+
+	private bool _playerNaArea = false;
+
+	public override void _Ready()
+	{
+		// Conectar os sinais via código (opcional, se não conectou pelo Editor)
+		BodyEntered += OnBodyEntered;
+		BodyExited += OnBodyExited;
+	}
+
+	public override void _Process(double delta)
+	{
+		// Verifica se o jogador está na área e pressionou a tecla
+		if (_playerNaArea && Input.IsActionJustPressed("interagir"))
+		{
+			FazerTeleporte();
+		}
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		if (body.IsInGroup("player"))
+		{
+			_playerNaArea = true;
+			GD.Print("Pressione 'E' para entrar");
+		}
+	}
+
+	private void OnBodyExited(Node2D body)
+	{
+		if (body.IsInGroup("player"))
+		{
+			_playerNaArea = false;
+		}
+	}
+
+	private void FazerTeleporte()
+	{
+		if (string.IsNullOrEmpty(ProximaCena))
+		{
+			GD.Print("Erro: Nenhuma cena selecionada!");
+			return;
+		}
+
+		// No C#, o GetTree() é um método
+		GetTree().ChangeSceneToFile(ProximaCena);
+	}
+}
